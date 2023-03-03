2d1dcb417ef1a23b2a65d6bc4b74d5e37cd1fb7c
Matt Cooper <vtbassmatt@gmail.com>
2023-03-03 06:03:46 -0500

    opening thoughts

---
+++ examples/00hello.py
@@ -0,0 +1,18 @@
+import skit
+
+# create a 2-card deck
+deck = skit.Deck(2)
+
+# set background color for all cards
+deck.background('white')
+
+# create a title area and set a unique title for both cards
+deck.layout(name='title', x=0, y=0, width=100, height=16)
+deck.text(['Hello', 'World'], layout='title')
+
+# make the first card have an additional graphic
+deck.layout(name='art', x=0, y=20, width=100, height=100)
+deck[0].png('graphic.png', layout='art')
+
+# create individual PNGs for cards
+deck.render_png('hello_{index}.png')
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Skit
+
+A tool for prototyping card games.
+
+Inspired by [squib][squib], but I could never quite get my head around Ruby.
+
+[squib]: https://github.com/andymeneely/squib
